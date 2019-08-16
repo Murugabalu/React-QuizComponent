bd927a08d219eb45d58731f1627ce5496e3d8368
Murugabalu <32058333+Murugabalu@users.noreply.github.com>
2019-08-16 17:15:01 +0530

    incorrectAnswer
    7th Module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,7 +10,7 @@ class QuizQuestion extends Component {
   }
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
-      this.setState({ inCorrectAnswer: false });
+      this.setState({ incorrectAnswer: false });
       this.props.showNextQuestionHandler();
     } else this.setState({ incorrectAnswer: true });
   }
